383e9a331d784f7f7c92ad54c4eb0db1f966f4b2
akmalropip <akmalropip@gmail.com>
2026-06-01 16:31:32 +0700

    membuat class toko elektronik

--- tugasPraktikum.cpp
+++ tugasPraktikum.cpp
@@ -4,3 +4,19 @@
 #include <array>
 #include <stdexcept>
 #include <vector>
+
+using namespace std;
+
+// 1. STRUKTUR KELAS TOKO ELEKTRONIK (OOP)
+class TokoElektronik {
+private:
+    // Atribut Private: Array berkapasitas 3 elemen (Enkapsulasi)
+    array<string, 3> etalase;
+
+public:
+    // Constructor: Mengisi data awal secara otomatis
+    TokoElektronik() {
+        etalase[0] = "Laptop HP Victus";
+        etalase[1] = "Router MikroTik";
+        etalase[2] = "Kabel LAN & RJ45";
+    }
